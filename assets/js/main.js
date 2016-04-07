ede721cb674c6e0390a366eb2fbee2ec249ccf81
Heather Brysiewicz <h.a.brysiewicz@gmail.com>
2016-04-07 11:51:08 -0700

    Fix for mobile rendering of header

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   var img = $('.img').css('background-image')
   img = img.replace('url(','').replace(')','')
-  $('.img').backstretch(img, {fade: 'slow'})
+  $('.img').backstretch(img, { fade: 'slow' })
 
   $(window).on('resize', function() {
     $('.img').backstretch('resize')
