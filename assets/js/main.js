914481bcb99fd61a78535c4700e5a6a025b41be4
Heather Brysiewicz <h.a.brysiewicz@gmail.com>
2016-04-07 12:28:31 -0700

    Mobile and image header fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   var img = $('.img').css('background-image')
-  img = img.replace('url(','').replace(')','')
+  img = img.match(/url\(\"?(.+)\"?\)/)[1]
   $('.img').backstretch(img, { fade: 'slow' })
 
   $(window).on('resize', function() {
